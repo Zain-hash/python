2584c59289e71836363f2aa026c668ed8d4487ce
Zain-hash <77725054+Zain-hash@users.noreply.github.com>
2024-02-22 18:58:55 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,11 @@
+#asking user for their name
+name = input("What is your name?")
+
+#ask user for their age
+age = input("How old are you?")
+
+#ask user for their location
+location = input("Where are you located?")
+
+#print personalized message
+print(f"Hello {name}! It's nice to meet you. You are {age} years old and located in {location}.")
